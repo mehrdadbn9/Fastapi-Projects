dc121c9693db8136e76b2631cf9f0818973fbcd5
mehrdadbn9 <mehrdadbiukian@gmail.com>
2022-07-15 10:54:21 +0430

    path and query params has been added

--- superbook2/router/blog_post.py
+++ superbook2/router/blog_post.py
@@ -1,6 +1,7 @@
 from enum import Enum
-from typing import Optional
-from fastapi import APIRouter, status, Response
+from typing import Optional, List
+from fastapi import APIRouter, Query, Body, Path
+from pydantic import BaseModel
 
 router = APIRouter(
     prefix='/blog',
@@ -8,6 +9,23 @@ router = APIRouter(
 )
 
 
-@router.post('/')
-def create_blog():
-    pass
+class BlogModel(BaseModel):
+    title: str
+    body: str
+    number_like: int
+    published: Optional[bool]
+
+
+@router.post('/new/{id}/blog/{blog_id}')
+def create_blog(blog: BlogModel, id: int, version: int = 1,
+                content: str = Body(..., max_length=50, min_length=10, regex='^[a-z\s]*$'),
+                v: Optional[List[str]] = Query(['1', '2']),
+                blog_id: int = Path(None, gt=6, le=8)):
+    return {'data': blog, "id": id, 'content': content, "version": v}
+
+
+@router.post('/new/{id}/comment/{comment_id}')
+def create_comment(blog: BlogModel, id: int,
+                   comment_id: int = Query(None, title='this is id comment', description="some description",
+                                           alias='commentId', deprecated=True)):
+    return {'blog': blog, "id": id, "comment_id": comment_id}
